8a1cf4a33a7176455300a203432064e4f622316e
AnandJatav0110 <anandjatav2702@gmail.com>
2025-07-10 13:26:36 +0530

    fix: deploy with backend deps and frontend build

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const path = require("path");
 dotenv.config();
 
 // connection URL
-const url = process.env.MONGO_URL || "mongodb://localhost:27017"; //
+const url = process.env.MONGO_URI || "mongodb://localhost:27017"; //
 const client = new MongoClient(url);
 
 // database
